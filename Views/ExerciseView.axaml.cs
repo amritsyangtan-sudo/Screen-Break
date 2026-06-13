bbba7aa1d66fc885f44e3fec922e3e154ca46eff
AmritS <amritsyangtan1@gmail.com>
2026-06-13 12:13:12 +0545

    Dashboard and Setting UI

---
+++ Views/ExerciseView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ScreenBreak;
+
+public partial class ExerciseView : UserControl
+{
+    public ExerciseView()
+    {
+        InitializeComponent();
+    }
+}
