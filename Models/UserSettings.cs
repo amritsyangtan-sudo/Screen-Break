c76a782e114851a952a3cd3b53a718d2dfecb67b
AmritS <amritsyangtan1@gmail.com>
2026-06-13 18:28:21 +0545

    Usersettings

---
+++ Models/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenBreak.Models
+{
+    public class UserSettings
+    {
+        public int WorkMinutes { get; set; } = 20;
+        public int BreakSeconds { get; set; } = 20;
+        public bool EnableSound { get; set; } = true;
+        public bool EnableNotifications { get; set; } = true;
+
+    }
+}
