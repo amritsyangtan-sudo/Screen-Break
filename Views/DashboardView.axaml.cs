a322027285b33947c37963ed890805a84b9e8625
AmritS <amritsyangtan1@gmail.com>
2026-06-13 20:48:38 +0545

    timer update

--- Views/DashboardView.axaml.cs
+++ Views/DashboardView.axaml.cs
@@ -1,13 +1,64 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using ScreenBreak.Services;
+using ScreenBreak.Models;
+using System;
+using Avalonia.Threading;
+
 
 namespace ScreenBreak.Views;
 
 public partial class DashboardView : UserControl
 {
+    private SettingsService _settingService = new SettingsService();
+    private UserSettings _userSettings = new UserSettings();
+    private int _remainingSeconds;
+    private DispatcherTimer _timer = new();
     public DashboardView()
     {
         InitializeComponent();
+        LoadSettings();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += TimerTick;
+    }
+
+    private void LoadSettings()
+    {
+      _userSettings = _settingService.LoadSettings();
+        _remainingSeconds = _userSettings.WorkMinutes * 60;
+        UpdateTimerDisplay();
+
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(_remainingSeconds);
+        TimerTextBlock.Text = time.ToString(@"mm\:ss");
+        TimerHintBlock.Text = time.ToString(@"mm\:ss");
+    }
+
+    private void TimerTick(object ? sender, EventArgs e)
+    {
+        if(_remainingSeconds > 0)
+        {
+            _remainingSeconds--;
+            UpdateTimerDisplay();
+        }
+        else
+        {
+            _timer.Stop();
+        }
+    }
+
+    private void StartButton_Click(object ? sender, RoutedEventArgs e)
+    {
+        _timer.Start();
+    }
+
+    private void PauseButton_Click(object ? sender, RoutedEventArgs e)
+    {
+        _timer.Stop();
     }
 }
